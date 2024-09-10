b6e51b7496a34fa5cdf11d0b9ef1566c043e23b2
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2024-09-11 00:43:34 +0300

    Lr9-10

---
+++ LR_9-10/9_10(10_1).pas
@@ -0,0 +1,18 @@
+﻿var a:array [1..6,1..8] of integer;
+b:array [1..6] of integer;
+i,j:integer;
+begin  
+
+  for i := 1 to 6 do
+  begin             
+    for j := 1 to 8 do                 
+    begin
+      A[i, j] := random(10);
+      write(A[i, j]:2);
+          if abs(a[i][1])>4 then b[i]:=a[i][1] else b[i]:=0;
+    end;
+    writeln;
+  end;
+
+    writeln(b);
+end.
---
+++ LR_9-10/9_10(10_2).pas
@@ -0,0 +1,21 @@
+﻿var
+  a: array [1..8, 1..6] of integer;
+  b: array [1..8] of integer;
+  i, j: integer;
+
+begin
+  for i := 1 to 8 do
+  begin             
+    for j := 1 to 6 do                 
+    begin
+      a[i, j] := random(40)-10;
+      write(A[i, j]:3);
+          if abs(a[i][1])>4 then b[i]:=a[i][1] else b[i]:=0;
+    end;
+    writeln;
+  end;
+  for i := 1 to 8 do
+    for j := 1 to 6 do
+      if a[i][j] < 0 then begin b[i] := -1; break end else b[i] := 1;
+  writeln(b);  
+end.
