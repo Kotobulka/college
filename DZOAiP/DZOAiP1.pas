52c4f6b57087822c2e28dc3e99eb44e1539160e0
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2024-09-12 10:02:36 +0300

    DZAOIP1

---
+++ DZOAiP/DZOAiP1.pas
@@ -0,0 +1,22 @@
+﻿ var
+  y,x: real;
+begin
+  x:=-12;
+  
+  while x<=1 do begin
+  if x < -10
+    then
+    y := ln(x)--x
+  else
+  if x < -1
+    then                   
+    y := cos(x)
+  else
+  if x >=-1
+    then
+    y := x*sqr(0.1*x)*-x-ln(x);
+ 
+  writeln(x:1:1,' = ',y);
+   x:=x+0.3;
+     end;
+end.
